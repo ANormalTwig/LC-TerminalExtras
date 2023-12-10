32be330519c446b28ad5234b103989d788463a62
nikolateslax <bobsklar12@gmail.com>
2023-12-09 21:28:01 -0600

    Added some new commands (#3)
    * Updated command set
    
    Added aliases:
    Teleport -> Tp
    Door -> Doors
    Lights -> Light
    
    Added new command
    Launch: pulls the lever. Pretty simple.
    
    Added new alias:
    Launch -> Go
    
    * Update .gitignore
    
    Exclude launch settings and data for Visual Studio
    
    * formatting
    
    ---------
    
    Co-authored-by: Twig <78218557+ANormalTwig@users.noreply.github.com>

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,18 @@
 ﻿using BepInEx;
-
-using LethalAPI.TerminalCommands;
 using LethalAPI.TerminalCommands.Models;
-using LethalAPI.TerminalCommands.Commands;
 
 namespace TerminalExtras;
 
 public static class PluginInfo {
 	public const string GUID = "twig.terminalextras";
 	public const string Name = "Terminal Extras";
-	public const string Version = "1.0.0";
+	public const string Version = "1.0.1";
 }
 
 [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-internal class Plugin: BaseUnityPlugin {
+internal class Plugin : BaseUnityPlugin {
 	private TerminalModRegistry commands;
-
 	public void Awake() {
 		commands = TerminalRegistry.RegisterFrom(new ExtraCommands());
 	}
 }
-
--- TerminalCommands.cs
+++ TerminalCommands.cs
@@ -13,6 +13,8 @@ public class ExtraCommands {
 
 		return "Toggled door.";
 	}
+	[TerminalCommand("Doors", clearText: false), CommandInfo("Toggles the door.")]
+	public string DoorsCommand() { return DoorCommand(); }
 
 	[TerminalCommand("Teleport", clearText: false), CommandInfo("Activate the teleporter.")]
 	public string TeleportCommand() {
@@ -25,15 +27,45 @@ public class ExtraCommands {
 		if (!teleporter.buttonTrigger.interactable) return "Teleporter is on cooldown!";
 
 		teleporter.buttonTrigger.onInteract.Invoke(GameNetworkManager.Instance.localPlayerController);
-		return "Teleporting player.";
+		return "Teleporting player";
 	}
 
 	[TerminalCommand("Lights", clearText: false), CommandInfo("Toggle the lights.")]
 	public string LightsCommand() {
-		InteractTrigger trigger = GameObject.Find("LightSwitch").GetComponent<InteractTrigger>();;
+		InteractTrigger trigger = GameObject.Find("LightSwitch").GetComponent<InteractTrigger>();
 		trigger.onInteract.Invoke(GameNetworkManager.Instance.localPlayerController);
 
-		return "Toggled lights.";
+		return "Toggled lights";
 	}
+	[TerminalCommand("Light", clearText: false), CommandInfo("Toggle the lights.")]
+	public string LightCommand() { return LightsCommand(); }
+
+	[TerminalCommand("Tp", clearText: false), CommandInfo("Activate the teleporter.")]
+	public string TeleportCommandShort() {
+		return this.TeleportCommand();
+	}
+
+	[TerminalCommand("Launch", clearText: false), CommandInfo("Pull the lever, Kronk!")]
+	public string LaunchCommand() {
+		const string alreadyTransitMessage = "Unable to comply. The ship is already in transit.";
+		GameObject leverObject = GameObject.Find("StartGameLever");
+		if (leverObject is null) return "!! Can't find StartGameLever !!";
+		StartMatchLever lever = leverObject.GetComponent<StartMatchLever>();
+		if (lever is null) return "!! Can't find StartMatchLever componen !!";
+
+		// Doors are enabled (on a moon), ship is either not landed or is leaving
+		if (StartOfRound.Instance.shipDoorsEnabled && !(StartOfRound.Instance.shipHasLanded || StartOfRound.Instance.shipIsLeaving)) { return alreadyTransitMessage; }
+		// Doors are disabled (in space), ship is in transit to another moon
+		if (!StartOfRound.Instance.shipDoorsEnabled && StartOfRound.Instance.travellingToNewLevel) { return alreadyTransitMessage; }
+
+		bool newState = !lever.leverHasBeenPulled;
+		lever.PullLever();
+		lever.LeverAnimation();
+		if (newState) lever.StartGame(); else lever.EndGame();
+		return "Initiating " + (lever.leverHasBeenPulled ? "landing" : "launch") + " sequence.";
+	}
+
+	[TerminalCommand("Go", clearText: false), CommandInfo("Pull the lever, Kronk!")]
+	public string GoCommand() { return LaunchCommand(); }
 }
 
